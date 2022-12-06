692121b67840b8bf5b328ffd709ae8ed81ba11d2
Azri Rahmat <azri-rahmat@Azris-MacBook-Pro.local>
2022-12-06 17:55:31 +0800

    basic functionality completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,63 @@
+import React, {useState,useEffect} from 'react';
 import './App.css';
-import GradeTracker from './components/GradeTracker';
+import {Card, CardHeader,CardContent} from "@mui/material"
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Students</h1>
-      <GradeTracker />
-    </div>
-  );
-}
+const App = () => {
+
+  const [studentData, setStudentData] = useState([])
+    const url = 'https://quanmgx57hjiqicdxgo2vzebqq0tghim.lambda-url.ap-southeast-1.on.aws'
+
+    useEffect(() =>{
+        axios.get(url)
+        .then(res => {
+            setStudentData(res.data)
+        })
+        .catch(err => {console.log(err)});
+    },[])
+
+    // console.table(studentData)
+
+    const newData = studentData.reduce((accumulator, currentValue) => {
+                const existing = accumulator[currentValue.student_id];
+                if(existing){
+                    accumulator[currentValue.student_id] = {
+                        courses: [...existing.courses, currentValue.course_id],
+                        gpa: (existing.gpa + currentValue.course_grade)/(existing.courses.length +1)
+                    }
+                }else{
+                    accumulator[currentValue.student_id] = {
+                        courses: [currentValue.course_id],
+                        gpa: currentValue.course_grade
+                    }
+                }
+                return accumulator;
+            }, {})
+            
+    return(
+      <div className='App'>
+        <h2>Students</h2>
+        <div style={{display: 'flex'}}>
+            {Object.keys(newData).map(studentId => {
+            const currentStudentData = newData[studentId];
+            return(
+              <Card>
+                <CardHeader title={studentId}/>
+                <CardContent>
+                  <div>
+                    Courses: {currentStudentData.courses.join(",")}
+                  </div>
+                  <div>
+                    GPA: {currentStudentData.gpa}
+                  </div>
+                </CardContent>
+              </Card>)
+              })}
+        </div>
+      </div>
+    )
+  }
+
+
 
 export default App;
