c641bf9337b296f09d8ac10736ab68c3210eb29c
Azri Rahmat <azri-rahmat@Azris-MacBook-Pro.local>
2022-12-07 16:57:49 +0800

    Loading page added when fetching the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
 import React, {useState,useEffect} from 'react';
 import './App.css';
 import {Card, CardHeader,CardContent} from "@mui/material"
+import LoadingPage from './components/LoadingPage'
 import axios from 'axios'
+import 'tachyons'
 
 const App = () => {
 
   const [studentData, setStudentData] = useState([])
+  const [loading, setLoading] = useState(true)
+
     const url = 'https://quanmgx57hjiqicdxgo2vzebqq0tghim.lambda-url.ap-southeast-1.on.aws'
 
     useEffect(() =>{
-        axios.get(url)
-        .then(res => {
-            setStudentData(res.data)
-        })
-        .catch(err => {console.log(err)});
+      let interval = setInterval(getData,2000)
+      setInterval(() =>{
+        clearInterval(interval)
+      },4000)
     },[])
 
-    // console.table(studentData)
+    function getData(){
+      axios.get(url, {name: "fetching"}, { timeout: 1000})
+        .then(res => {
+            setStudentData(res.data)
+            setLoading(false)
+        })
+        .catch(err => {
+          console.log(err)
+          setLoading(true)
+        });
+        
+    }
 
+    console.table(studentData)
     const newData = studentData.reduce((accumulator, currentValue) => {
                 const existing = accumulator[currentValue.student_id];
                 if(existing){
@@ -36,24 +51,35 @@ const App = () => {
             
     return(
       <div className='App'>
-        <h2>Students</h2>
-        <div style={{display: 'flex'}}>
+        { loading ? 
+        <LoadingPage/>
+        :
+        <div>
+        <h2 className="tc">Students</h2>
+        <div className="mw9 center ph5-ns">
             {Object.keys(newData).map(studentId => {
             const currentStudentData = newData[studentId];
             return(
-              <Card>
+              <Card className="br3 pa3 ma2 bw2 shadow-5 fl w5" >
+                <CardContent>
+                  <div>Student</div>
+                </CardContent>
                 <CardHeader title={studentId}/>
                 <CardContent>
-                  <div>
-                    Courses: {currentStudentData.courses.join(",")}
+                  <div className="textWrap">
+                    {currentStudentData.courses.join(",")}
                   </div>
-                  <div>
-                    GPA: {currentStudentData.gpa}
+                  <div className="textWrap">
+                    GPA: {(currentStudentData.gpa).toFixed(2)}
                   </div>
                 </CardContent>
               </Card>)
               })}
         </div>
+        </div>
+        
+      }
+        
       </div>
     )
   }
---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function LoadingPage(){
+
+return(
+    <p>Page is loading please wait</p>
+)
+}
+
+export default LoadingPage;
